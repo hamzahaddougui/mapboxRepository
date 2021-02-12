fa91ab4a1b522b7e4142166a8370d6593233aded
AhmedL-dev <laadraouiahmed@gmail.com>
2021-02-12 13:24:33 +0100

    Added drawer layout

--- pages/test.js
+++ pages/test.js
@@ -1,17 +1,64 @@
-import {Button} from '@material-ui/core'
-import Drawer from '../src/components/Drawer' 
-import {useState} from 'react'
+import { Button } from "@material-ui/core";
+import Drawer from "../src/components/Drawer";
+import Drawer2 from "../src/components/Drawer2";
+import { useState } from "react";
 
 const test = () => {
-    
-    const [open, setopen] = useState(true)
+  const [open, setopen] = useState(true);
 
-    return (
-        <div style={{height: "100vh", display: "flex", justifyContent:"center", alignItems:"center", backgroundColor:"purple"}}>
-        <Button onClick={() => {setopen(!open)}}>Click me!</Button>
-        <Drawer open={open} setOpen={setopen}/>
+  return (
+    <div
+    //   style={{
+    //     height: "100vh",
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     backgroundColor: "purple",
+    //   }}
+    >
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "purple",
+          position: "fixed",
+          zIndex: 1,
+        }}
+      >
+        <button onClick={() => console.log("CLICK")}>CLICK ME</button>
+      </div>
+      <div style={{ height: "100%", width: "100vw", overflow: "hidden" }}>
+        <div style={{ opacity: 0, height: "80vh", width: "100vw", zIndex: -1 }}>qdqsds</div>
+        <div
+          style={{
+            backgroundColor: "white",
+            height: "100%",
+            width: "100vw",
+            position: "relative",
+            zIndex: 1,
+          }}
+        >
+          {[...new Array(10)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join("\n")}
         </div>
-    )
-}
+      </div>
 
-export default test
+      {/* <Button
+        onClick={() => {
+          setopen(!open);
+        }}
+      >
+        Click me!
+      </Button>
+      <Drawer open={open} setOpen={setopen} /> */}
+    </div>
+  );
+};
+
+export default test;
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,36 +1,51 @@
-import { makeStyles } from '@material-ui/core/styles';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import { Collapse } from '@material-ui/core';
-
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import { Collapse } from "@material-ui/core";
+import "./Drawer.module.css";
+import { useState } from "react";
 
 const useStyles = makeStyles({
-  drawer:{
-    height: 600,
+  drawer: {
+    height: 800,
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: '20%',
+    borderRadius: "20%",
     padding: 40,
-  }
+    transition: "smooth",
+  },
 });
 
-export default function Drawer({open,setOpen}) {
-    const classes = useStyles();
-    return(
-      <Collapse in={open} collapsedHeight={200} >
-        <SwipeableDrawer style={{borderRadius: "20%", minHeight: 200}}
-              anchor="bottom"
-              open={open}
-              onClose={()=>{setOpen(!open)}}
-              onOpen={()=>{setOpen(!open)}}
-            >
-            <div className={classes.drawer}>
-              <h1>Hello</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero voluptatum dicta at tenetur deserunt, dolor fugiat voluptatibus blanditiis quibusdam. Ullam magnam reiciendis atque maiores incidunt itaque cupiditate harum officiis veniam.</p>
-            </div>
-        </SwipeableDrawer>
-      </Collapse>
-    )
-  }
-  
+export default function Drawer({ open, setOpen }) {
+  const classes = useStyles();
+  const [height, setHeight] = useState(600);
+
+  return (
+    <SwipeableDrawer
+      style={{ borderRadius: "50%", minHeight: 50 }}
+      className={classes.sDrawer}
+      anchor="bottom"
+      open={open}
+      onClose={e => {
+        // setOpen(!open);
+        console.log("Close", e);
+      }}
+      hysteresis={0}
+      onOpen={() => {
+        console.log("TOCHED");
+        setOpen(!open);
+      }}
+      SwipeAreaProps={{ style: { color: "red" } }}
+    >
+      <div style={{ height }} className={classes.drawer}>
+        <h1>Hello</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero voluptatum dicta at
+          tenetur deserunt, dolor fugiat voluptatibus blanditiis quibusdam. Ullam magnam reiciendis
+          atque maiores incidunt itaque cupiditate harum officiis veniam.
+        </p>
+      </div>
+    </SwipeableDrawer>
+  );
+}
