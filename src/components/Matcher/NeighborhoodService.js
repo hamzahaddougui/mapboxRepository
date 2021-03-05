cf59352e4d97ce14ddcd0587c2a5d15653c9c326
AhmedL-dev <laadraouiahmed@gmail.com>
2021-03-05 15:16:48 +0100

    updating api middleware

--- src/components/Matcher/NeighborhoodService.js
+++ src/components/Matcher/NeighborhoodService.js
@@ -1,59 +1,57 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 import { apiCallBegan } from "../../services/api";
 
 const url = "/matcher/run";
 
 const slice = createSlice({
+  name: "neighborhoodResult",
 
-    name: 'neighborhoodResult',
-
-    initialState: {
-        matched: [],
-        loading: false,
-        lastFetch: null
+  initialState: {
+    matched: [],
+    loading: false,
+    lastFetch: null,
+  },
+  reducers: {
+    neighborhoodRequested: (state, action) => {
+      state.loading = true;
     },
-    reducers: {
-        neighborhoodRequested: (state, action) => {
-            state.loading = true;
-        },
-        neighborhoodRequestFailed: (state, action) => {
-            state.loading = false;
-        },
-        neighborhoodReceived: (state, action) => {
-            state.matched = action.payload;
-            state.loading = false;
-            state.lastFetch = Date.now();
-            console.log(state.matched)
-        },
-    }
-
+    neighborhoodRequestFailed: (state, action) => {
+      state.loading = false;
+    },
+    neighborhoodReceived: (state, action) => {
+      state.matched = action.payload;
+      state.loading = false;
+      state.lastFetch = Date.now();
+      console.log(state.matched);
+    },
+  },
 });
 
 export const {
-    neighborhoodRequested,
-    neighborhoodRequestFailed,
-    neighborhoodReceived
+  neighborhoodRequested,
+  neighborhoodRequestFailed,
+  neighborhoodReceived,
 } = slice.actions;
 
 export default slice.reducer;
 
 export const loadMatched = () => (dispatch, getState) => {
-    // if (isCached(getState().modules.filters)) return;
-    console.log("load Filters ...");
-    const data = [];
+  // if (isCached(getState().modules.filters)) return;
+  console.log("load Filters ...");
+  const data = {};
 
-    data.filters = getState().modules.filter.priorities;
+  data.filters = getState().modules.filter.priorities;
 
-    console.log(data);
+  console.log(data);
 
-    dispatch(
-      apiCallBegan({
-        url,
-        onStart: neighborhoodRequested.type,
-        onSuccess: neighborhoodReceived.type,
-        onError: neighborhoodRequestFailed.type,
-        data,
-        method: "POST"
-      }),
-    );
-  };
+  dispatch(
+    apiCallBegan({
+      url,
+      onStart: neighborhoodRequested.type,
+      onSuccess: neighborhoodReceived.type,
+      onError: neighborhoodRequestFailed.type,
+      data,
+      method: "POST",
+    }),
+  );
+};
